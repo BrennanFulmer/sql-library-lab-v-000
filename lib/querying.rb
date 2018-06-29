544a07d38de7566f7b60f2f43550117e4a8c9832
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-29 15:40:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.author_id = characters.author_id
   WHERE characters.species = 'human' 
-  ORDER BY COUNT(*)"
+  ORDER BY COUNT(*) DESC"
   #LIMIT 1"
 end
 
